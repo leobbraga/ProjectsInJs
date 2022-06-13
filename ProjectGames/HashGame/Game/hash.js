95427aac60ce46fdf27d001c0e85b0c4d18d5843
leonardo Barbosa <barbosaleonardo466@gmail.com>
2022-06-13 15:26:28 -0300

    first commit

---
+++ ProjectGames/HashGame/Game/hash.js
@@ -0,0 +1,59 @@
+//NamePlayers
+var Player1NameTag = document.getElementById('Player_one'); 
+var Player2NameTag = document.getElementById('Player_two');   
+
+console.log(Player1NameTag);
+console.log(Player2NameTag);
+
+
+function Buttonstart() {
+document.getElementById('DIV_WINDOW').remove();
+}
+/*
+function decidewinner(Player, computerChoise) {
+    var rpsDatabase = {
+        'rock': { 'scissors': 1, 'rock': 0.5, 'paper': 0 },
+        'paper': { 'rock': 1, 'paper': 0.5, 'scissors': 0 },
+        
+    };
+
+function message() {
+var ImageDatabaseName = {
+    'Beta': document.getElementById('Beta').src,
+    'Chad': document.getElementById('chad').src
+}
+document.getElementById('Chad').remove();
+document.getElementById('Beta').remove();
+
+var IconBeta = document.createElement('div');
+var IconChad = document.createElement('div');
+
+
+document.getElementById('flex-box-rps-div').appendChild(IconChad);
+document.getElementById('flex-box-rps-div').appendChild(IconBeta);
+
+}
+
+*/
+
+
+
+
+
+//HashGame
+/*
+function HashGame(Player1, Player2) {
+    console.log(Player1);
+    console.log(Player2);
+
+    var ChoisePlayer1, ChoisePlayer2;
+    ChoisePlayer1 = Player1.id;
+    ChoisePlayer2 = Player2.id;
+
+    results = decidewinner(ChoisePlayer1, ChoisePlayer2);
+    console.log(results);
+
+    message = finalMessage(results); 
+    hashFrontEnd(Player1.id, Player2.id, message);
+}
+*/
---
+++ pagProject/Pagina1/databank.sql
@@ -0,0 +1,9 @@
+create table email_client;
+
+
+
+
+
+
+
+
